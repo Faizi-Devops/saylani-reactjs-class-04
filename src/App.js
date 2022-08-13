5880db6591d5df4230f33cf87e2c55bcbef23e08
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-13 04:09:48 -0700

    react-js-work"

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Update from './Hooks/useState/Update';
+
+function App() {
+  return (
+    <div>
+      <Update />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hooks/useState/Update.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import "./style.css";
+
+let Update = () =>{
+  let [students,setStudents]=useState([
+    {
+      name:"Faizan Ali",
+      roll:49546,
+      class:"BSCS",
+      batch:"2019-2023",
+
+    },
+    {
+      name:"Muhammad Arslan",
+      roll:49566,
+      class:"BSIT",
+      batch:"2018-2022",
+
+    },
+    {
+      name:"Faisal Nawaz",
+      roll:49555,
+      class:"BS Economics",
+      batch:"2014-2018",
+
+    },
+    {
+      name:"Naveed Sarwar",
+      roll:49666,
+      class:"BS Bioinformatics",
+      batch:"2012-2016",
+
+    },
+  ]);
+  return(
+    <div>
+
+      <div className="A"><h1>List of Students</h1></div>
+      <table className="table">
+ 
+    <tr>
+      <th scope="col">Student Name</th>
+      <th scope="col">Student Roll</th>
+      <th scope="col">Student Class</th>
+      <th scope="col">Student Batch</th>
+    </tr>
+
+    
+
+    {students.map((value,index)=>{
+      return  <tr>
+      <td scope="col">{value.name}</td>
+      <td scope="col">{value.roll}</td>
+      <td scope="col">{value.class}</td>
+      <td scope="col">{value.batch}</td>
+    </tr>
+      
+    })}
+  
+  </table>
+
+
+      
+    </div>
+
+  )
+}
+export default Update;
